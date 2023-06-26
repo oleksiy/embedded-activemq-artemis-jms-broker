39ed9c8e269028316ed7bbc58580b66b31590bb3
Oleksiy Blavat <oblavat@invoca.com>
2023-06-26 00:12:45 -0400

    consumer added - everything compiles via plugin and cmdline - all deps are visible

---
+++ src/main/java/com/codeaches/activmq/embedded/JmsConsumer.java
@@ -0,0 +1,17 @@
+package com.codeaches.activmq.embedded;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JmsConsumer {
+
+    Logger log = LoggerFactory.getLogger(JmsConsumer.class);
+
+    @JmsListener(destination = "${spring.jms.template.default-destination}")
+    public void receive(String message) {
+        log.info("Received message='{}'", message);
+    }
+}
