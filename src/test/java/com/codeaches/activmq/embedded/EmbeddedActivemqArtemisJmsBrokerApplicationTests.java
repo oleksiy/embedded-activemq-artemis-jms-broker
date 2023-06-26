90c010a1689ecffbaec11f293459eb3cdc18e4e5
Oleksiy Blavat <oblavat@invoca.com>
2023-06-25 23:57:02 -0400

    initial commit - everything compiles

---
+++ src/test/java/com/codeaches/activmq/embedded/EmbeddedActivemqArtemisJmsBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeaches.activmq.embedded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbeddedActivemqArtemisJmsBrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
